e82640dfc16974b45d00f2d441adde9043b3e599
Pronob <pronobmozumder.info@gmail.com>
2020-12-11 02:47:40 +0600

    Done

--- Middle of Three.py
+++ Middle of Three.py
@@ -1,13 +1,29 @@
-def reverseWord(s):
-    s = list(s)
-    l = len(s)
+# User function Template for python3
 
-    i = 0
-    j = l - 1
-    p = ''
-    while (i < j):
-        s[i], s[j] = s[j], s[i]
-        i += 1
-        j -= 1
-    p=''.join([i for i in s])
-    return p
+class Solution:
+    def middle(self, A, B, C):
+        m = max(A, B, C)
+        mi = min(A, B, C)
+
+        if mi < A < m:
+            r = A
+        elif mi < B < m:
+            r = B
+        else:
+            r = C
+        return r
+
+        # code here
+
+
+# {
+#  Driver Code Starts
+# Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        A, B, C = map(int, input().strip().split())
+        ob = Solution()
+        print(ob.middle(A, B, C))
+# } Driver Code Ends
